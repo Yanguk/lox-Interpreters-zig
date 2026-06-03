602b072074d8d1315bb3b4a473ce0b54f23cf20e
Yanguk <yanguk@duck.com>
2026-06-03 22:01:35 +0900

    feat: scanner, main

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lox-Interpreters-zig
+
+https://github.com/jwmerrill/zig-lox
+
+https://craftinginterpreters.com/the-lox-language.html
--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,16 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
 const Io = std.Io;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
+pub fn runFile(path: []const u8) !void {
+    std.debug.print("runFile, path: {s}\n", .{path});
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn runPrompt() !void {
+    std.debug.print("not yet implement!, fn runPrompt", .{});
+
+    return;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+fn run() !void {
+    std.debug.print("not yet implement!, fn run", .{});
 }
